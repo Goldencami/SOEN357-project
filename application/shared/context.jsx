63d942e5a40171934ea3fdb747ee0ccc733e5adb
Camila Guzman <camila.guzman023@gmail.com>
2025-04-08 23:17:51 -0400

    setup electron desktop app

--- application/shared/context.jsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-
-export const roundContext = createContext(); // Gets round #
-export const buttonContext = createContext(); // when going to another page, use this context to notify children to save data
